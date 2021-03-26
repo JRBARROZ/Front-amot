d819259b0e6f97c01bdb25d5cb8591f90f9e0b28
Murielson <jhonsnoow32@gmail.com>
2021-03-26 17:51:50 -0300

    the monster is getting out the jail

--- server.js
+++ server.js
@@ -3,12 +3,25 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const WebSocket = require('ws');
+const mongoose = require('mongoose');
 const wss = new WebSocket.Server({ server:server})
+const db = 'mongodb+srv://barroz:6969@cluster0.vb8kd.mongodb.net/Amot?retryWrites=true&w=majority';
+const Thing = require('./models/thingSchema.js');
 let conn = [];
 let clients = {};
 let socketId = () => {
     return new Date().getTime()
 }
+
+mongoose.connect(db, {
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopology:true,
+    useFindAndModify:false,
+}).then(() =>{
+    console.log('Connection Success')
+}).catch((err) => console.log('Fail :' + err));
+ 
 app.use('/css',express.static(__dirname + '/css'));
 app.use('/images',express.static(__dirname + '/images'));
 app.use('/js',express.static(__dirname + '/js'));
@@ -16,7 +29,6 @@ app.use('/js',express.static(__dirname + '/js'));
 wss.on('connection', function connection(ws){
     console.log('Novo cliente conectado!')
     // console.log(ws);
-    console.log(ws);
     conn.push(ws);
 
     let id = socketId()
@@ -42,7 +54,34 @@ wss.on('connection', function connection(ws){
 // },1000);
 app.get('/waterlevel', function(req, res){
     res.sendStatus(200);
+
     console.log(req.query);
+    let arr = req.query;
+    console.log(arr.thing_id.length);
+    for(let i = 0; i < arr.thing_id.length; i++){
+        Thing.find({thing_id:req.query.thing_id[i]}, function(err, docs){
+            if(docs.length == 0){
+                let storeThing = new Thing({
+                    thing_id: req.query.thing_id[i],
+                    water_level: req.query.water_level[i]
+                });
+                storeThing.save();
+            }else{
+                Thing.findOne({thing_id:req.query.thing_id[i]}, function (err, doc){
+                    doc.water_level = req.query.water_level[i];
+                    doc.save();
+                });
+            }
+        });
+    }
+    setTimeout(()=> {
+        Thing.find({}, function(err, things){
+            conn.forEach(ws => {
+                ws.send(JSON.stringify(things));
+            })
+        });
+    },2000);
+    // console.log(req.query.thing_id);
     // console.table(conn);
     // console.log(req.query.thing_id);
     // let ws;
@@ -50,9 +89,7 @@ app.get('/waterlevel', function(req, res){
     //     ws = conn[i];
     //     ws.send(new ArrayBuffer(req.query)); 
     // }
-    conn.forEach(ws => {
-        ws.send(JSON.stringify(req.query));
-    })
+
     // console.log(req.query);
     // console.log(conn);
 });
